f4a9e2312e1bf92c370e04dcb291052d52d43ad1
Akash-Appinventiv <akash.agrawal@appinventiv.com>
2023-02-22 18:10:03 +0530

    added homescreen to router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import screenNames from '../utils/screenNames';
+import HomeScreen from '../modules/home/screens';
 
 const Stack = createNativeStackNavigator();
 
 const RootRouter = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>{/* <Stack.Screen /> */}</Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name={screenNames.HOMESCREEN} component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
